63818f250087e19f9f6ed99d3ba2f63d1e483d78
jasLogic <jaslo@jaslogic.tech>
2019-09-12 20:47:55 +0200

    Update setup.py
    - add additional information for pypi and packaging

--- setup.py
+++ setup.py
@@ -11,9 +11,23 @@ setup(
     name = "mipea",
     version = "0.0.1",
     description = "minimalistic peripheral access for the Raspberry Pi",
+    keywords = "c peripheral access Raspberry Pi mipea",
     author = "jasLogic",
     author_email = "jaslo@jaslogic.tech",
-    # url =
+    url = "https://github.com/jasLogic/mipeapy",
+    project_urls = {
+        "Bug Tracker": "https://github.com/jasLogic/mipeapy/issues",
+        "Source Code": "https://github.com/jasLogic/mipeapy",
+        #"Download": "https://github.com/jasLogic/mipeapy/releases"
+    },
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: C",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware"
+    ],
+
     packages = ["mipea"],
     ext_modules = [gpio],
 )
